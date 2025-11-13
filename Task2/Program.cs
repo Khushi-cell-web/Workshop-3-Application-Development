b65308374e83380382ca12e046baeb88666e370f
Khushi-cell-web <khushibohora692@gmail.com>
2025-11-13 10:06:59 +0545

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create object and call method
+            NullOperators nullOps = new NullOperators();
+            nullOps.PerformNullChecks();
+        }
+    }
+}
